c8448e65e9688a2c2f5e6f7f5bc355bb1751867c
Jacob Cunningham <jacob.cunningham@live.com>
2022-12-29 12:48:13 -0600

    Add Logging Behavior

---
+++ src/Ferdinand.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ferdinand.Application.Behaviors;
+
+public sealed class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehavior(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var response = await next();
+
+            _logger.LogDebug("APP: {RequestType} => OK", typeof(TRequest).Name);
+
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
--- src/Ferdinand.Application/ServiceCollectionExtensions.cs
+++ src/Ferdinand.Application/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using Ferdinand.Application.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +9,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services
-            .AddMediatR(typeof(AssemblyMarker));
+            .AddMediatR(typeof(AssemblyMarker))
+            .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
         return services;
     }
