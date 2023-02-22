3178803be5d87cce1d46dbe21de95554bc9c26af
Jacob Cunningham <jacob.cunningham@live.com>
2023-02-21 21:14:05 -0600

    remove comment

--- src/Ferdinand.Api/Program.cs
+++ src/Ferdinand.Api/Program.cs
@@ -27,7 +27,6 @@ builder.Services.AddVersionedApiExplorer(
     });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
