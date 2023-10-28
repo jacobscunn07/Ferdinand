42d01be2c7ce794c4d12b51446027e05e8ca7480
Jacob Cunningham <jacob.cunningham@live.com>
2023-10-28 11:45:30 -0500

    refactor GetColorValidatorTests for readability

--- tests/Ferdinand.Testing/Builders/GetColorQueryBuilder.cs
+++ tests/Ferdinand.Testing/Builders/GetColorQueryBuilder.cs
@@ -5,4 +5,5 @@ namespace Ferdinand.Testing.Builders;
 public static class GetColorQueryBuilder
 {
     public static GetColorQuery CreateQuery(Guid colorKey) => new(colorKey);
+    public static GetColorQuery CreateQuery(string colorKey) => new(Guid.Parse(colorKey));
 }
